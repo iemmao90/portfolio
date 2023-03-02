e5f9f5206345876d7ce4f9036c738417944a4a81
Emma Olsson <emma.olsson22@learnet.se>
2023-03-02 11:39:00 +0100

    hej

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <section className="main-container">
         <div className="left-container">left-container hej</div>
         <div className="right-top-container">right-top-container hej</div>
-        <div className="right-bottom-container">right-bottom-container</div>
+        <div className="right-bottom-container">right-bottom-container hej</div>
       </section>
     </>
   );
